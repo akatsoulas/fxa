7fc9a251931221fb623dc5f373a00b3611373ebc
Jared Hirsch <ohai@6a68.net>
2019-10-10 10:23:28 -0700

    test(payments): sketch out tests for amplitude lib

---
+++ packages/fxa-payments-server/server/lib/amplitude.test.js
@@ -0,0 +1,11 @@
+describe('amplitude', () => {
+  describe('transforms and logs valid data for all planned ping formats', () => {
+    // TODO enumerate all planned pings, ensure they validate.
+  });
+
+  // Most of the validation is tested by the fxa-shared lib.
+  // But the fuzzy event matching should be tested here.
+  describe('fuzzy event matching works for all planned ping formats', () => {
+    // TODO enumerate all planned events, ensure they match.
+  });
+});
