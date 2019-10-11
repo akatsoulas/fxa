e8f2016f39035fdb0c52da92fb55980cac2bc705
Jared Hirsch <ohai@6a68.net>
2019-10-11 08:49:54 -0700

    fix(tests): add no-op test to prevent CI failure

--- packages/fxa-payments-server/server/lib/amplitude.test.js
+++ packages/fxa-payments-server/server/lib/amplitude.test.js
@@ -1,6 +1,25 @@
+const amplitude = require('./amplitude');
+const assert = require('chai').assert;
+const sinon = require('sinon');
+
+const log = {
+  info: sinon.spy(),
+};
+const amplitudeConfig = {
+  disabled: false,
+};
+
+const testEvents = ['amplitude.sub.relyingPartyView'];
+// TODO: mock out logger to examine the fired log things.
+
+// TODO: test the snakeCase conversions: amplitude.sub.relyingPartyView -> amplitude.fxa_subscribe.relying_party_view
+
 describe('amplitude', () => {
   describe('transforms and logs valid data for all planned ping formats', () => {
     // TODO enumerate all planned pings, ensure they validate.
+    it('needs a placeholder or will fail', () => {
+      assert(true);
+    });
   });
 
   // Most of the validation is tested by the fxa-shared lib.
